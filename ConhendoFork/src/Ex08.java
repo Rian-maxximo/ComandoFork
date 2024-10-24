692bb3451c363d20ba5c8f5e319b9258b40bba25
Giovanna <0000985407@senaimgaluno.com.br>
2024-10-24 15:17:34 -0300

    Enviando arquivo

--- ConhendoFork/src/Ex08.java
+++ ConhendoFork/src/Ex08.java
@@ -1,9 +1,26 @@
+
 /*Somatório com Condição: Calcule a soma de todos os números inteiros de 1 a N, 
  * mas somente se N for par.
  */
+import java.util.Scanner;
+
 public class Ex08 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner =new Scanner(System.in);
+		
+		System.out.println("digite um numero inteiro");
+		int n = scanner.nextInt();
+		if (n % 2 == 0 ) {
+			int soma = (n*(n+1))/2;
+			System.out.println("a soma dos numeros inteiros de 1 a " + n + " é "  + soma);
+		}
+		
+	
+	else{
+		System.out.println("O número digitado não é par");}
+
+		scanner.close();
 
 	}
 }
